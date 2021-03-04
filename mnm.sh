bd363b2b0c18940e7c443b104432167c7165bd68
Cedric Fung <cedric@vec.io>
2021-03-05 06:22:15 +0800

    bash script for automatic installation

---
+++ mnm.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# mnm is a process monitor & notifier
+# notifications delivered to Mixin Messenger
+# curl -sSL https://mnm.sh/MM-WEBHOOK-TOKEN | bash
+
+TOKEN=MM-WEBHOOK-TOKEN
+BIN=/usr/bin/mnm
+ETC=/etc/default/mnm
+
+case "$(uname -m)" in
+    x86_64)     arch="x64"  ;;
+    aarch64)    arch="arm64";;
+esac
+
+case "$(uname -s)" in
+    Linux*)     bin="linux-${arch}";;
+    Darwin*)    bin="macos-${arch}";;
+    *)          bin="linux-${arch}";;
+esac
+
+RAW="https://raw.githubusercontent.com/cedricfung/mnm/master/bin/pgn-${bin}"
+
+echo "curl -L ${RAW} -o /tmp/mnm"
+echo
+curl -L "${RAW}" -o /tmp/mnm
+echo
+
+if [-f "${BIN}"]
+then
+    echo "${BIN} INSTALLED"
+    exit 0
+fi
+
+if [-f "${ETC}"]
+then
+    echo "${ETC} CONFIGURED"
+    exit 0
+fi
+
+if file /tmp/mnm | grep -q "executable"
+then
+    echo "chmod +x /tmp/mnm"
+    chmod +x /tmp/mnm
+    echo
+
+    echo "sudo mv /tmp/mnm /usr/bin/"
+    sudo mv /tmp/mnm /usr/bin/
+    echo
+
+    echo "echo ${TOKEN} | sudo tee ${ETC}"
+    echo "${TOKEN}" | sudo tee "${ETC}"
+    echo
+else
+    echo "OOPS"
+fi
+
+echo "OK"
